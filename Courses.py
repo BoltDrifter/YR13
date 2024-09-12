1063cb1ae0a63a35c7f5d9092d33c0165ce0066b
BoltDrifter <baldeep.gd14@gmail.com>
2024-09-12 22:18:51 +0400

    ds

--- Courses.py
+++ Courses.py
@@ -1,5 +1,5 @@
 class Lesson:
-    def __init__(self,Title,Min,r):
+    def __init__(self,Title,Min,r) -> None:
         self.__LessonTitle = Title
         self.__DurationMinutes = Min
         self.__RequiresLab = r
@@ -15,25 +15,19 @@ class Assessment:
         print(f"The Title of the assessment is : {self.__AssesmentTitle}")
         print(f"Max Marks : {self.__MaxMarks}")
 class Course:
-    def __init__(self,CourseTitle,MaxS,NoL,L,M,R,T,MS):
-        Lesson.__init__(self,L,M,R)
-        Assessment.__init__(self,T,MS)
+    def __init__(self,CourseTitle,MaxS,NoL):
         self.__CourseTitle = CourseTitle
         self.__MaxStudents = MaxS
         self.__NumberofLessons = NoL
         self.__CourseAssessment = Assessment
-        self.__CourseLesson = [0 for x in range(2)]
+        self.__CourseLesson = [0 for x in range(50)]
     def AddLesson(self,T,M,R):
         self.__CourseLesson.append(Lesson(T,M,R))
     def AddAssessment(self,T,M):
         self.__CourseAssessment.append(Assessment(T,M))
     def OutputCourseDetails(self):
-        print(f"Course Title : {self.__CourseTitle}")
-        print(f"Max Number of Students: {self.__MaxStudents}") 
-        print(f"Number of Lessons: {self.__NumberofLessons}")
-        Lesson.OutputLessonDetails(self)
-        Assessment.OutputAssessmentDetails(self)
+        print(self.__CourseTitle, "Maximum number: ", self.__MaxStudents)
 
-First = Course("CS101", 399, 20, "OOP", 30, False, "OOP Basics", 300)
-
-First.OutputCourseDetails(  )
+First = Course("CS101", 399, 20)
+First.AddLesson("OOP",90,False)
+First.OutputCourseDetails()
