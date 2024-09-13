a1d29116d8722f6a3e247c7b3dfda1c964a0e1e6
BoltDrifter <baldeep.gd14@gmail.com>
2024-09-13 16:51:51 +0400

    jk

--- Courses.py
+++ Courses.py
@@ -1,26 +1,30 @@
 class Lesson:
+    
     def __init__(self,Title,Min,r) -> None:
         self.__LessonTitle = Title
         self.__DurationMinutes = Min
         self.__RequiresLab = r
+
     def OutputLessonDetails(self):
         print(f"The Lesson Title is: {self.__LessonTitle}")
         print(f"The Duration is: {self.__DurationMinutes} minutes")
         print(f"Lab Required: {self.__RequiresLab}")
+
 class Assessment:
     def __init__(self,Title,Max):
         self.__AssesmentTitle = Title
         self.__MaxMarks = Max
+
     def OutputAssessmentDetails(self):
         print(f"The Title of the assessment is : {self.__AssesmentTitle}")
         print(f"Max Marks : {self.__MaxMarks}")
 class Course:
-    def __init__(self,CourseTitle,MaxS,NoL):
+    def __init__(self,CourseTitle,MaxS):
         self.__CourseTitle = CourseTitle
         self.__MaxStudents = MaxS
-        self.__NumberofLessons = NoL
+        self.__NumberofLessons = 0
+        self.__CourseLesson = []
         self.__CourseAssessment = Assessment
-        self.__CourseLesson = [0 for x in range(50)]
     def AddLesson(self,T,M,R):
         self.__NumberofLessons += 1
         self.__CourseLesson.append(Lesson(T,M,R))
@@ -28,9 +32,11 @@ class Course:
         self.__CourseAssessment = Assessment(T,M)
     def OutputCourseDetails(self):
         print(self.__CourseTitle, "Maximum number: ", self.__MaxStudents)
-        for x in range(len(self.__CourseLesson)):
-            print(self.__CourseLesson[x].OutputLessonDetails()) 
+        for i in range(self.__NumberofLessons):
+            self.__CourseLesson[i].OutputLessonDetails() 
 
-First = Course("CS101", 399, 20)
-First.AddLesson("OOP",90,False)
+
+First = Course("CS101", 399)
+First.AddLesson("OOP",90,True)
+First.AddLesson("Plagirism",90,False)
 First.OutputCourseDetails()
