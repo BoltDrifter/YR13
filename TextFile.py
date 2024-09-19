d024999e5a146a89a2176cb43510ea27b4b7d363
BoltDrifter <Baldeep.gd14@gmail.com>
2024-09-19 09:39:58 +0400

    sdas

--- TextFile.py
+++ TextFile.py
@@ -1,6 +1,16 @@
 FileHandle = open("sample.txt", "w")
-for i in range(100000):
-    FileHandle.write(f"{i}\n")
+for i in range(1,11):
+    ask = input("Enter a Subject: ")
+    FileHandle.write(f"Subject {i}: {ask}\n")
 
 
 FileHandle.close()
+
+
+FileHandleRead = open("sample.txt", "r")
+
+ThisLine = FileHandleRead.readline()
+while len(ThisLine) > 0:
+    ThisLine = FileHandleRead.readline()
+    print(ThisLine)
+FileHandleRead.close()
