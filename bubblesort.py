f44973f3b44adfeba85a5ecd9be52430d5da8f85
BoltDrifter <Baldeep.gd14@gmail.com>
2024-09-26 22:22:24 +0400

    Algorithms

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+Array = [62,31,11,34,66,32]
+MaxIndex = 6
+n = MaxIndex - 1 
+NoMoreSwaps = False
+
+while NoMoreSwaps == False:
+    NoMoreSwaps == True
+    for j in range(n):
+        if Array[j] > Array[j+1]:
+            Temp = Array[j]
+            Array[j] = Array[j+1]
+            Array[j+1] = Temp
+            NoMoreSwaps = False
+    n = n - 1
+
+print(Array)
