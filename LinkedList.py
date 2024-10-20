1aa376f46408a6f8f0027504f4f961a81c743152
BoltDrifter <baldeep.gd14@gmail.com>
2024-10-20 17:46:53 +0400

    fds

--- LinkedList.py
+++ LinkedList.py
@@ -1,51 +1,89 @@
-NULLPOINTER = -1
+nullpointer = -1
 
 class ListNode :
   def __init__(self) :
     self.Data = "-"
-    self.Pointer = NULLPOINTER
+    self.Pointer = nullpointer
 
 # Initialize the Linked List
 
 def InitialiseList() :
+    global List
     List = [ListNode() for i in range(8)]
-    StartPointer = NULLPOINTER 
+    StartPointer = nullpointer
     FreeListPtr = 0 
     for Index in range(7) : 
         List[Index].Pointer = Index + 1
         print("[",Index,"] ",List[Index].Data,"  ",List[Index].Pointer) 
-    List[7].Pointer = NULLPOINTER 
+    List[7].Pointer = nullpointer
     print("[",7,"] " ,List[7].Data,"  ",List[7].Pointer)
-    return(List, StartPointer, FreeListPtr)
+    return(StartPointer, FreeListPtr)
 
 # Insertion of Data 
 
-def InsertNode(List, StartPointer, FreeListPtr, NewItem) :
-    s="a"
-    if FreeListPtr != NULLPOINTER :
+def InsertNode(Startpointer, FreeListPtr) :
+    ask = int(input("Enter Data: "))
+    if FreeListPtr != nullpointer :
         NewNodePtr = FreeListPtr
-        List[NewNodePtr].Data = NewItem
+        List[NewNodePtr].Data = ask
         FreeListPtr = List[FreeListPtr].Pointer
-        PreviousNodePtr = NULLPOINTER
+        PreviousNodePtr = nullpointer
         ThisNode = Startpointer
-        while ThisNode != NULLPOINTER and List[ThisNode].Data < NewItem:
+        while ThisNode != nullpointer and List[ThisNode].Data < ask:
             PreviousNodePtr = List[ThisNode].Pointer
             ThisNode = List[ThisNode].Pointer
-        if PreviousNodePtr == NULLPOINTER:
+        if PreviousNodePtr == nullpointer:
             List[NewNodePtr].Pointer = Startpointer
             Startpointer = NewNodePtr
         else:
             List[NewNodePtr].Pointer = List[PreviousNodePtr].Pointer
             List[PreviousNodePtr].Pointer = NewNodePtr
+        if List[FreeListPtr].Data == "-":
+            List[PreviousNodePtr].Pointer = -1
     else:
         print("No space for more data")
-        s = "full"
-        return(List,Startpointer,FreeListPtr,s)
+    for Index in range(7) : 
+        List[Index].Pointer = Index + 1
+        print("[",Index,"] ",List[Index].Data,"  ",List[Index].Pointer) 
+    List[7].Pointer = nullpointer
+    print("[",7,"] " ,List[7].Data,"  ",List[7].Pointer)
+    return(Startpointer,FreeListPtr)
+    
+def deleteNode(Startpointer,FreeListPtr):
+    Pre = List[Startpointer - 1].Pointer 
+    FreeListPtr = FreeListPtr - 1
+    List[Startpointer].Pointer = Pre
+    print(Startpointer, FreeListPtr)
+    return(Startpointer,FreeListPtr)
 
+a,b = InitialiseList()
 
-a,b,c = InitialiseList()
-print(a)
-for x in range(len(a)):
-    ask = int(input("Enter Data: "))
-    InsertNode(a, b, c, ask)
-print(a)
+m = a
+s = b
+
+Choice = int(input("""
+1. Push
+2. Pop
+3. Output
+4. End
+                   
+"""))
+while Choice != 4:
+    if Choice == 1:
+        c,d = InsertNode(m,s)
+        m = c
+        s = d
+    if Choice == 2:
+       m,s= deleteNode(m,s)
+    if Choice == 3:
+        # Print()
+        pass
+    if Choice == 4:
+        quit()
+    Choice = int(input("""
+1. Push
+2. Pop
+3. Output
+4. End
+                           
+"""))
--- QueueLinked.py
+++ QueueLinked.py
@@ -8,47 +8,91 @@ class Node:
 def InitializeList():
     global List 
     StartPointer = 0
-    FreeListPtr = -1
+    EndPointer = -1
     NumberinQueue = 0
     List = [Node() for i in range(8)]
-    for index in range(len(List)):
+    for index in range(len(List)-1):
         List[index].Pointer = index + 1
         print(f"[{index}]  {List[index].Data} {List[index].Pointer}")
     List[7].Pointer = nullpointer
-    print("[",7,"] " ,List[7].Data,"  ",List[7].Pointer)
-    return(List, StartPointer, FreeListPtr,NumberinQueue)
+    print(f"[7] {List[7].Data} {List[7].Pointer}")
+    return(StartPointer, EndPointer,NumberinQueue)
 
-def Push(StartPointer, FreeListPtr,NumberInQueue):
+def Push(StartPointer, EndPointer ,NumberInQueue):
     global MaxQueue 
     MaxQueue = len(List)
-    Pre = FreeListPtr
+    Pre = EndPointer + 1
     ask = int(input("Enter Data: "))
     if NumberInQueue < MaxQueue:
-        FreeListPtr += 1
-        if FreeListPtr > MaxQueue:
-            FreeListPtr = 0
-        List[FreeListPtr].Data = ask
+        EndPointer += 1
+        if EndPointer > MaxQueue:
+            EndPointer = 0
+        List[EndPointer].Data = ask
         NumberInQueue += 1
-    if List[FreeListPtr + 1].Data == "-":
-        List[FreeListPtr].Pointer = -1
-        List[FreeListPtr - 1].Pointer = Pre
-    elif NumberInQueue >= MaxQueue:
-        print("Queue Full !")
+        if EndPointer < 7:
+            if List[EndPointer + 1].Data == "-":
+                List[EndPointer].Pointer = -1
+                List[EndPointer - 1].Pointer = Pre
+        else:
+            print("Queue Full !")
     print(NumberInQueue,MaxQueue)
     print("   ")
     for i in range(len(List)):
         print(f"[{i}]   {List[i].Data} {List[i].Pointer}")
     print("   ")
     print(f"Top Pointer: {StartPointer}")
-    print(f"End Pointer: {FreeListPtr}")
-    return(StartPointer,FreeListPtr,NumberInQueue)
+    print(f"End Pointer: {EndPointer}")
+    return(StartPointer,EndPointer,NumberInQueue)
 
 
+def Pop(StartPointer, EndPointer, NumberInQueue):
+    if NumberInQueue == MaxQueue:
+        StartPointer = StartPointer + 1
+        List[EndPointer].Pointer = StartPointer
+        if EndPointer > MaxQueue:
+            EndPointer = -1
+        print(EndPointer)
+        EndPointer += 1
+        NumberInQueue = MaxQueue - 1
+    print("   ")
+    for i in range(len(List)):
+        print(f"[{i}]   {List[i].Data} {List[i].Pointer}")
+    print("   ")
+    print(f"Top Pointer: {StartPointer}")
+    print(f"End Pointer: {EndPointer}")
+    return(StartPointer,EndPointer,NumberInQueue)
 
+def Print():
+    for i in range(len(List)):
+        print(f"[{i}]   {List[i].Data}  {List[i].Pointer}")
 
-
-
-a,b,c,d = InitializeList()
-
-f,g,h = Push(b,c,d)
-Push(f,g,h) 
+a,b,c = InitializeList()
+m = a
+s = b
+f = c
+Choice = int(input("""
+1. Push
+2. Pop
+3. Output
+4. End
+                   
+"""))
+while Choice != 4:
+    if Choice == 1:
+        k,d,e = Push(m,s,f)
+        m = k 
+        s = d
+        f = e
+    if Choice == 2:
+       m,s,f = Pop(m,s,f)
+    if Choice == 3:
+        Print()
+    if Choice == 4:
+        quit()
+    Choice = int(input("""
+1. Push
+2. Pop
+3. Output
+4. End
+                           
+"""))
